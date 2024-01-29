d2f7b3626e40e07f50f992643c885de5a549a3ce
Rohanraje Sawant <rohanrajesawant@gmail.com>
2024-01-29 07:04:39 -0500

    first commit for challenge

---
+++ src/main/java/com/example/coderbytechallenge/CoderByteChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.example.coderbytechallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoderByteChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoderByteChallengeApplication.class, args);
+    }
+
+}
